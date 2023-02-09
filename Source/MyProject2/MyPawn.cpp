587c86fdcfdc7ad736084f2486c7d3f8e00dc4e7
Constantin <constantin.lenglos@epitech.eu>
2023-02-09 17:09:00 +0100

    [sOURCE]

---
+++ Source/MyProject2/MyPawn.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyPawn.h"
+#include <Camera/CameraComponent.h>
+
+
+// Sets default values
+AMyPawn::AMyPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set this pawn to be controlled by the lowest-numbered player
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	// Create a dummy root component we can attach things to.
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+	// Create a camera and a visible object
+	UCameraComponent* OurCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("OurCamera"));
+	OurVisibleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("OurVisibleComponent"));
+	// Attach our camera and visible object to our root component. Offset and rotate the camera.
+	OurCamera->SetupAttachment(RootComponent);
+	OurCamera->SetRelativeLocation(FVector(-500.0f, 0.0f, 150.0f));
+	OurCamera->SetRelativeRotation(FRotator(0.0f, 0.0f, 0.0f));
+	OurVisibleComponent->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void AMyPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	// Handle movement based on our "MoveX" and "MoveY" axes
+	{
+		if (!CurrentVelocity.IsZero())
+		{
+			FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+			SetActorLocation(NewLocation);
+		}
+	}
+
+}
+
+// Called to bind functionality to input
+void AMyPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+	InputComponent->BindAxis("MoveX", this, &AMyPawn::Move_XAxis);
+	InputComponent->BindAxis("MoveY", this, &AMyPawn::Move_YAxis);
+
+}
+
+void AMyPawn::Move_XAxis(float AxisValue)
+{
+	// Move at 100 units per second forward or backward
+	CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 2000.0f;
+}
+
+void AMyPawn::Move_YAxis(float AxisValue)
+{
+	// Move at 100 units per second right or left
+	CurrentVelocity.Y = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 1000.0f;
+}
+
+
